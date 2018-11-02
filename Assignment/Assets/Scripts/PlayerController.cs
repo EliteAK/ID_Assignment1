1616eb75b58d79a05825be72bd6ec1956eff2d26
Ashley Kelly (s5071846) <s5071846@bournemouth.ac.uk>
2018-11-02 14:51:27 +0000

    Placed collectables around the level

--- Assignment/Assets/Scripts/PlayerController.cs
+++ Assignment/Assets/Scripts/PlayerController.cs
@@ -12,12 +12,14 @@ public class PlayerController : MonoBehaviour {
 	float dist;
 	RaycastHit hit;
 
+	public bool isMoving = false;
+
 	Quaternion newRotation;
 	float rotSpeed = 5.0f ;
 
 	// Use this for initialization
 	void Start () {
-		//myAnim = GetComponent<Animator> ();
+		myAnim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -25,23 +27,28 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
 
-			if (Physics.Raycast(ray,out hit)) {
+			if (Physics.Raycast (ray, out hit)) {
 				agent.SetDestination (hit.point);
-				//myAnim.SetBool ("isRunning", true);
+				myAnim.SetBool ("isRunning", true);
+				isMoving = true;
 			}
 		}
 
-		Vector3 relativePos = hit.point - transform.position;
-		newRotation = Quaternion.LookRotation (relativePos, Vector3.up);
-		newRotation.x = 0.0f;
-		newRotation.z = 0.0f;
+		if (isMoving == true) 
+		{
+			Vector3 relativePos = hit.point - transform.position;
+			newRotation = Quaternion.LookRotation (relativePos, Vector3.up);
+			newRotation.x = 0.0f;
+			newRotation.z = 0.0f;
 
-		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, rotSpeed * Time.deltaTime);
+			transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, rotSpeed * Time.deltaTime);
 
-		dist = Vector3.Distance (hit.point, transform.position);
-		//Debug.Log ("Distance" + dist);
-		if (dist < 1f) {
-			//myAnim.SetBool ("isRunning", false);
+			dist = Vector3.Distance (hit.point, transform.position);
+			//Debug.Log ("Distance" + dist);
+			if (dist < 1f) {
+				myAnim.SetBool ("isRunning", false);
+				isMoving = false;
+			}
 		}
 	}
 }
